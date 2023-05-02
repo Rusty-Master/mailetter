fa0101b9c1cb387ddbad0937097804b1cf3d25e0
RustMaster <dawidmajka@protonmail.com>
2023-05-02 16:47:19 +0200

    Email client and mocking

---
+++ src/email_client.rs
@@ -0,0 +1,111 @@
+use reqwest::{Client, Url};
+use secrecy::{ExposeSecret, Secret};
+use serde::Serialize;
+
+use crate::domain::SubscriberEmail;
+
+#[derive(Serialize)]
+struct SendEmailRequest {
+    from: String,
+    to: String,
+    subject: String,
+    html_body: String,
+    text_body: String,
+}
+
+pub struct EmailClient {
+    http_client: Client,
+    base_url: String,
+    sender: SubscriberEmail,
+    authorization_token: Secret<String>,
+}
+
+impl EmailClient {
+    pub fn new(
+        base_url: String,
+        sender: SubscriberEmail,
+        authorization_token: Secret<String>,
+    ) -> Self {
+        EmailClient {
+            http_client: Client::new(),
+            base_url,
+            sender,
+            authorization_token,
+        }
+    }
+
+    pub async fn send_email(
+        &self,
+        recipient: SubscriberEmail,
+        subject: &str,
+        html_content: &str,
+        text_content: &str,
+    ) -> Result<(), reqwest::Error> {
+        let url = format!("{}/email", self.base_url);
+
+        let request_body = SendEmailRequest {
+            from: self.sender.as_ref().to_owned(),
+            to: recipient.as_ref().to_owned(),
+            subject: subject.to_owned(),
+            html_body: html_content.to_owned(),
+            text_body: text_content.to_owned(),
+        };
+        self.http_client
+            .post(&url)
+            .header(
+                "X-Postmark-Server-Token",
+                self.authorization_token.expose_secret(),
+            )
+            .json(&request_body)
+            .send()
+            .await?;
+
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use fake::{
+        faker::{
+            internet::en::SafeEmail,
+            lorem::en::{Paragraph, Sentence},
+        },
+        Fake, Faker,
+    };
+    use secrecy::Secret;
+    use wiremock::{matchers::any, Mock, MockServer, ResponseTemplate};
+
+    use crate::{domain::SubscriberEmail, email_client::EmailClient};
+
+    #[tokio::test]
+    async fn send_email_fires_a_request_to_base_url() {
+        // Given
+        let mock_server = MockServer::start().await;
+        let sender = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let email_client = EmailClient::new(mock_server.uri(), sender, Secret::new(Faker.fake()));
+
+        // Email data is fake and not hardcoded to point that it is not important.
+        // What is important is that our client is sending email.
+        let subscriber_email = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let subject: String = Sentence(1..2).fake();
+        let content: String = Paragraph(1..10).fake();
+
+        Mock::given(any())
+            .respond_with(ResponseTemplate::new(200))
+            .expect(1)
+            .mount(&mock_server)
+            .await;
+
+        // When
+        let _ = email_client
+            .send_email(subscriber_email, &subject, &content, &content)
+            .await;
+
+        // Then
+
+        // No need for assertions
+        // When mock_server goes out of scope "expectations" are verified,
+        // If not met will cause panic!
+    }
+}
